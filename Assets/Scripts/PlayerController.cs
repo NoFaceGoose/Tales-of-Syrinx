99533a3c2ce8929177dcee271714fc51f31dfc5a
Alt3rmis <18111272170@163.com>
2021-11-10 10:16:14 +0000

    Revert "change control"
    This reverts commit 59d1defaa3b4ceeb00d45c93a2591cbded19dd59.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,8 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController player;
 
-<<<<<<< Updated upstream
     public float moveSpeed;
     public float jumpForce = 5.0f;
-=======
-    public float jumpForce = 10.0f;
-
->>>>>>> Stashed changes
     public float gravity;
     public GameObject Bullet;
     public int keys = 0;
@@ -25,22 +20,18 @@ public class PlayerController : MonoBehaviour
     private float inputX;
     private bool isGrounded = false;
 
-<<<<<<< Updated upstream
 
 
     void Awake()
     {
         player = this;
     }
-=======
-    public bool canDoubleJump = false; //Make the player able to double jump
->>>>>>> Stashed changes
 
     void Start()
     {
         rg = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
-
     void Update()
     {
         rg.velocity = new Vector3(inputX * moveSpeed, rg.velocity.y, 0);
@@ -53,25 +44,32 @@ public class PlayerController : MonoBehaviour
         inputX = value.ReadValue<Vector2>().x;
     }
 
-    void jump()
-    {
-        rg.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-    }
-
     public void OnJump(InputAction.CallbackContext value)
     {
         if (isGrounded)
         {
-            Debug.Log("jump!");
-            jump();
+            rg.velocity = new Vector3(rg.velocity.x, 0, 0);
+            // rg.velocity = new Vector2(rg.velocity.x, jumpForce);
+            rg.AddForce(jump * jumpForce, ForceMode.Impulse);
             canDoubleJump = true;
         }
         else if (canDoubleJump)
         {
-            canDoubleJump = false;
             Debug.Log("double jump!");
-            jump();
+            rg.velocity = new Vector3(rg.velocity.x, 0, 0);
+            // rg.velocity = new Vector2(rg.velocity.x, jumpForce);
+            rg.AddForce(jump * jumpForce, ForceMode.Impulse);
+            canDoubleJump = false;
         }
+        // if (isGrounded)
+        // {
+        //     jumpCount = 0;
+        // }
+        // if (jumpCount < 3)
+        // {
+        //     rg.velocity = new Vector2(rg.velocity.x, jumpForce);
+        //     jumpCount += 1;
+        // }
     }
 
     public void OnFire(InputAction.CallbackContext value)
