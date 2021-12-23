e3fec1caef712e51cabda62fcde05254cc429fae
Alt3rmis <18111272170@163.com>
2021-12-23 00:48:15 +0000

    Added pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // should be updated to input system
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // Time.timeScale can be used to create bullet time
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        // Debug.Log("LoadMenu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
