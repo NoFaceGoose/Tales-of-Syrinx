269a14ebbf20d5e2be2d9a5ad8e3cd1b10495731
NoFaceGoose <c.peng@hss20.qmul.ac.uk>
2022-01-04 22:22:08 +0000

    key sprite

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -2,52 +2,30 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    public float TumbleSpeed;
+    public float radian = 0;
+    public float perRadian = 0.1f;
+    public float radius = 0.1f;
+    private Vector3 _originalPosition;
 
-    private bool _keyB = false;
-    private int _keyIndex = 0;
-    private Vector3 _offset;
-    private Transform _playerTrans;
-    private Rigidbody _rigidbody;
-
-    void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-        _playerTrans = GameObject.FindWithTag("Player").transform;
-        _offset = new Vector3(-0.6f, 0, 0);
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        _rigidbody.angularVelocity = Random.insideUnitSphere * TumbleSpeed;
+        _originalPosition = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (_keyB == false)
-        {
-            if (_rigidbody.angularVelocity.z < 0.5)
-            {
-                _rigidbody.angularVelocity = Random.insideUnitSphere * TumbleSpeed;
-            }
-        }
-        else
-        {
-            if (_playerTrans != null)
-            {
-                transform.position = _playerTrans.position + _offset * _keyIndex;
-            }
-        }
+        radian += perRadian;
+        float dy = Mathf.Cos(radian) * radius;
+        transform.position = _originalPosition + new Vector3(0, dy, 0);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            _keyB = true;
-            _keyIndex = PlayerController.PlayerInstance.SetKeys();
+            PlayerController.PlayerInstance.SetKeys();
+            Destroy(gameObject);
         }
     }
 }
