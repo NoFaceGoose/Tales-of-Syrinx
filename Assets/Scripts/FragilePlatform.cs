5c7c7b51c468409381bbe356d086b97e8fc071eb
NoFaceGoose <c.peng@hss20.qmul.ac.uk>
2022-01-13 11:01:21 +0000

    Fragile platform shake and sprite, tutorial level adjustment

--- Assets/Scripts/FragilePlatform.cs
+++ Assets/Scripts/FragilePlatform.cs
@@ -1,7 +1,32 @@
 using UnityEngine;
+using System.Collections;
 
 public class FragilePlatform : MonoBehaviour
 {
+    public Vector3 shakeRate = new Vector3(0.05f, 0.05f, 0.05f);
+    public float shakeTime = 1f;
+    public float shakeDertaTime = 0.05f;
+    public float returnTime = 3f;
+
+    public void Shake()
+    {
+        StartCoroutine(Shake_Coroutine());
+    }
+
+    public IEnumerator Shake_Coroutine()
+    {
+        var oriPosition = gameObject.transform.position;
+        for (float i = 0; i < shakeTime; i += shakeDertaTime)
+        {
+            gameObject.transform.position = oriPosition +
+                Random.Range(-shakeRate.x, shakeRate.x) * Vector3.right +
+                Random.Range(-shakeRate.y, shakeRate.y) * Vector3.up +
+                Random.Range(-shakeRate.z, shakeRate.z) * Vector3.forward;
+            yield return new WaitForSeconds(shakeDertaTime);
+        }
+        gameObject.transform.position = oriPosition;
+    }
+
     void Start()
     {
 
@@ -16,14 +41,15 @@ public class FragilePlatform : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player") && other.gameObject.transform.position.y > gameObject.transform.position.y + other.gameObject.transform.localScale.y / 5)
         {
-            Invoke("Destroy", 0.5f);
+            Shake();
+            Invoke("Destroy", shakeTime);
         }
     }
 
     void Destroy()
     {
         gameObject.SetActive(false);
-        Invoke("Return", 3f);
+        Invoke("Return", returnTime);
     }
 
     void Return()
