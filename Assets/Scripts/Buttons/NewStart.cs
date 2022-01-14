9e5dd4a32076106f95e4a7b2deddfc4cf7322684
NoFaceGoose <c.peng@hss20.qmul.ac.uk>
2022-01-14 00:51:00 +0000

    delete discard scripts

--- Assets/Scripts/Buttons/NewStart.cs
+++ Assets/Scripts/Buttons/NewStart.cs
@@ -3,18 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class NewStart : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     public void OnClick()
     {
         SceneManager.LoadScene(1);
